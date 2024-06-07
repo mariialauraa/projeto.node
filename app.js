5e201a94b302d31814ec59e38995a48f6eb150a0
Maria Laura <mlauraifg@gmail.com>
2024-06-07 09:15:21 -0300

    Fazendo a lógica da Busca

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path')
 const db = require('./db/connection')
 const bodyParser = require('body-parser')
 const Job = require('./models/Job')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 // iniciar o servidor
 const PORT = 3000;
@@ -36,14 +38,34 @@ db
 
 // routes
 app.get('/', (req, res) => {
-    Job.findAll({order: [
-        ['createdAt', 'DESC']
-    ]})
-    .then(jobs => {        
-        res.render('index', {
-            jobs
-        })        
-    })
+
+    let search = req.query.job // botão Procurar
+    let query = '%'+search+'%' //PH -> PHP, Word -> WordPress
+
+    if (!search) {
+        Job.findAll({order: [
+            ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {        
+            res.render('index', {
+                jobs
+            })        
+        })
+        .catch(err => console.log(err))
+    } else {
+        Job.findAll({
+            where: {title: {[Op.like]: query}},
+            order: [
+                ['createdAt', 'DESC']
+        ]})
+        .then(jobs => {        
+            res.render('index', {
+                jobs, search
+            })        
+        })
+        .catch(err => console.log(err))
+    }
+
 })
 
 // jobs routes
